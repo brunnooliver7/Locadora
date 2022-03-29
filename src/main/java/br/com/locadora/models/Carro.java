b49edbc298fb9c62512d81e8606e006d3f1aaa82
Bruno Oliveira <brunno.oliver7@gmail.com>
2022-03-28 21:57:48 -0300

    get carro by id

--- src/main/java/br/com/locadora/models/Carro.java
+++ src/main/java/br/com/locadora/models/Carro.java
@@ -1,6 +1,7 @@
 package br.com.locadora.models;
 
 public class Carro {
+  public Long id;
   public String nome;
   public String placa;
 }
--- src/main/java/br/com/locadora/resources/CarrosResource.java
+++ src/main/java/br/com/locadora/resources/CarrosResource.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
@@ -23,6 +24,37 @@ public class CarrosResource {
     lista.add((carro1));
 
     return lista;
+  }
+
+  @GET
+  @Path("/{id}")
+  @Produces(value = MediaType.APPLICATION_JSON)
+  public Carro obterCarro(@PathParam("id") Long id) {
+    
+    Carro carro1 = new Carro();
+    carro1.id = 1L;
+    carro1.nome = "Carro 1";
+    carro1.placa = "ABC-1234";
+    
+    Carro carro2 = new Carro();
+    carro2.id = 2L;
+    carro2.nome = "Carro 2";
+    carro2.placa = "ABC-1234";
+    
+    Carro carro3 = new Carro();
+    carro3.id = 3L;
+    carro3.nome = "Carro 2";
+    carro3.placa = "ABC-1234";
+
+    ArrayList<Carro> lista = new ArrayList<>();
+    lista.add(carro1);
+    lista.add(carro2);
+    lista.add(carro3);
+
+    return lista.stream()
+      .filter(carro -> carro.id.equals(id))
+      .findFirst()
+      .orElse(null);
   }
 
 }
